e93303c8d8156bdbca64764b0e404afecf5108f2
dingyaoyang <74450260+dingyaoyang@users.noreply.github.com>
2021-05-29 22:16:45 +0800

    Add files via upload

---
+++ week12/Result.java
@@ -0,0 +1,31 @@
+package week12;
+
+import java.awt.CardLayout;
+
+public class Result {
+	private String courseName;
+	private float avg;
+	
+	public Result() {
+		super();
+	}
+	
+	public Result(String courseName, float avg) {
+		super();
+		this.courseName = courseName;
+		this.avg = avg;
+	}
+	
+	public float calAvg(){
+		
+		
+		return avg;
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ week12/Student.java
@@ -0,0 +1,54 @@
+package week12;
+
+public class Student {
+	private String id;
+	private String name;
+	private String gender;
+	private float grade;
+	public Student() {
+		super();
+	}
+	public Student(String id, String name, String gender) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+	}
+	public Student(String id, String name, String gender, float grade) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.grade = grade;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public float getGrade() {
+		return grade;
+	}
+	public void setGrade(float grade) {
+		this.grade = grade;
+	}
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+
+}
